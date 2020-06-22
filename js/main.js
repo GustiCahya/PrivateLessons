6c84123a7eb72fc6fe46bc2e06354ee8211e0a35
GustiCahya <gusticahyax@yahoo.com>
2020-06-22 17:21:14 +0800

    NavbarIntersecting

--- js/main.js
+++ js/main.js
@@ -16,3 +16,16 @@ document.addEventListener('DOMContentLoaded', function(){
         M.Carousel.getInstance($('.carousel')).next();
     }, 2000);
 })
+
+const navbarIntersecting = new IntersectionObserver(function(entries, observer){
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            $('nav').style.top = "-50%";
+        }else{
+            $('nav').style.top = "0";
+        }
+    })
+}, {
+    rootMargin: '0px 0px -400px 0px'
+});
+navbarIntersecting.observe($('.teach'));
